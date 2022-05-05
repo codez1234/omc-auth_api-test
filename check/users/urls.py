de8d7e3147290de4c74b7e8a5c70a863927303e8
codez1234 <104984012+codez1234@users.noreply.github.com>
2022-05-05 17:22:26 +0530

    Delete check directory

--- check/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from users.views import *
-urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-    # path('changepassword/', UserChangePasswordView.as_view(), name='changepassword')
-]
